3c35c5798ba2122b5545a9869a11f993c5c09f36
steven-shuken <srshuken@gmail.com>
2022-07-10 18:38:46 -0400

    Created an Rcpp package skeleton within this project, moved all the directories up a level, and moved "RcppPackageSkeleton.R" into the R folder.

---
+++ R/RcppPackageSkeleton.R
@@ -0,0 +1,4 @@
+# This is all the code needed to add boilerplate files to your project (not created as a package!) To make it into a package.
+# Note: This makes the package inside the project you already created, if you created one first. There is no RStudio project (.Rproj).
+library(Rcpp)
+Rcpp.package.skeleton("RcppPackage")
